2dbb709d8f67570fd41c9bddf57643aa4c289a2d
yangmingzhe <yangmingzhe@kuaishou.com>
2021-02-22 18:09:18 +0800

    feat: support sass.

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,5 +1,5 @@
 <template>
-  123
+  Hello, Vue 3.0 + Vite
 </template>
 
 <script>
@@ -10,13 +10,15 @@ export default defineComponent({
 })
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 200px;
 }
 </style>
